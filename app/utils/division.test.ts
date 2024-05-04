b8b12843e16fe20516508cc1bb8271b679993761
Vanessa Quintero <vpquintero@uninorte.edu.co>
2024-05-04 08:08:24 -0500

    Feature: adding all changes into next app

---
+++ app/utils/division.test.ts
@@ -0,0 +1,4 @@
+import { division } from "./division";
+test("test", () => {
+    expect(division(10, 2)).toBe(5);
+})
---
+++ app/utils/division.ts
@@ -0,0 +1,6 @@
+export function division(a: number, b: number) {
+    if(b == 0) {
+        return "el divisor debe ser diferente a cero";
+    }
+    return a / b
+}
